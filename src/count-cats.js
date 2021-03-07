c4e476cfb74371dd86f8c284c7ede560dc176ca6
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-07 22:49:54 +0100

    feat: implement function to count the cats hidden

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let res = 0;
+  let symbol = '^^'
+  for (let i = 0; i < matrix.length; i++) {
+      for (let j=0; j < matrix[i].length; j++)
+      if (matrix[i][j] === symbol) {
+        res += 1
+      }
+    }
+  return res;
 };

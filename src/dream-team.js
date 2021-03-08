d9c1fde6ce89284930c6abf585cdcae307defcd3
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-08 23:42:35 +0100

    feat: create team name based on the members names

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!members) {
+    return false;
+  }
+  let res = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      res.push(members[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  res.sort();
+  return res.join('');
+}

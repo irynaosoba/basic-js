8d0370a69722f2eb68c6b5e5a2299073912165c2
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-09 17:31:33 +0100

    feat:  implement function that returns the time of the year from the Date object

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let season = '';
+  if (!date){
+    return 'Unable to determine the time of year!'
+  } if (!date.getTime()){
+    throw new Error();
+  }
+  else if (date.getMonth() === 11 || date.getMonth() <= 1) {
+    season = 'winter'
+  } else if (date.getMonth() > 1 && date.getMonth() <= 4) {
+    season = 'spring'
+  } else if (date.getMonth() > 4 && date.getMonth() <= 7) {
+    season = 'summer'
+  } else {
+    season = 'fall'
+  }
+  return season;
 };

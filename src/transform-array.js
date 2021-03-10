6c2dd6b68a532962189ab7222b055c4a0b9460e6
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-10 12:35:43 +0100

    feat: implement the function to transform array based on the control sequences

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let res = [];
+    if (!arr) {
+        throw new Error();
+    }
+    let discarded = false;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            discarded = true;
+            i++;
+        } else if (arr[i] === '--discard-prev') {
+            if (i > 0 && !discarded) {
+                res.splice(res.length - 1, 1);
+            }
+            discarded = false;
+        } else if (arr[i] === '--double-next') {
+            if (i < arr.length - 1) {
+                res.push((arr[i + 1]));
+            }
+            discarded = false;
+        } else if (arr[i] === '--double-prev') {
+            if (i > 0 && !discarded) {
+                res.push(arr[i - 1]);
+            }
+            discarded = false;
+        } else {
+            res.push(arr[i]);
+            discarded = false;
+        }
+    }
+    return res;
 };

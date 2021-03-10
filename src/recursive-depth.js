53db5ad252d778721c3e7c9a91521b19ed9fc2b0
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-10 21:59:43 +0100

    feat: implement the class method that returns array depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let maxArrDepth = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])){
+        maxArrDepth = Math.max(maxArrDepth, 1 + this.calculateDepth(arr[i]))
+      }
+    }
+    return maxArrDepth;
   }
 };

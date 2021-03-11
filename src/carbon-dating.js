d81adc76e4951c16089b0c7d589069c4a82e299d
Iryna <48490280+irynaosoba@users.noreply.github.com>
2021-03-11 20:30:10 +0100

    feat: implement function to determine the age of archaeological finds

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (!sampleActivity || typeof sampleActivity !== "string") {
+        return false;
+    }
+    let sampleActivityNumber = parseFloat(sampleActivity);
+    if (isNaN(sampleActivityNumber) || typeof sampleActivityNumber !== "number"
+        || sampleActivityNumber <= 0 || sampleActivityNumber > MODERN_ACTIVITY) {
+        return false;
+    }
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let age = Math.log(MODERN_ACTIVITY / sampleActivityNumber) / k;
+    return Math.ceil(age);
 };
